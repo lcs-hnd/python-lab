3f3c888eadeb416d695d09830375a7ea1849c3df
Lucas Honda <103671864+lcs-hnd@users.noreply.github.com>
2025-07-15 12:10:16 -0400

    docs: create one more solution for 347

--- algorithms/347-top-k-frequent-elements/347-v3-top-k-frequent-elements.py
+++ algorithms/347-top-k-frequent-elements/347-v3-top-k-frequent-elements.py
@@ -5,9 +5,13 @@ nums = [1,1,1,2,2,3]
 k = 2 
 
 def freq(nums, k):
-  count = Counter(nums)
+  count = Counter(nums) # new class from collections, creating an instance
 
+  heap = []
+  for number, freq in count.items(): # unpacking the newly made tuple from the count object
+    heapq.heappush(heap, (freq, number)) # pushing to the list now used as a heap where it's sorted by freq
+
+    if len(heap) > k: # prioritize the larget frequencies
+      heapq.heappop(heap) 
   
-
-
-
+  return [number for freq, number in heap] # loops through heap where each item is a tuple but only number is returned
--- python-syntax/modules/heapq/heappush.py
+++ python-syntax/modules/heapq/heappush.py
@@ -1,4 +1,8 @@
-import heapq
+# a heap is a special kinf of list where python keeps the smallest element at the top (index 0)
+# when heappush"ing" python will automatically reorder the list to keep the smallest on top
+
+# heaps can have anything pushed to them as long as python can compare them
+# if you want to push objects then you have to define how they're to be compared in the respective classes
 
 # full sig: heapq.heappush(heap, item)
   # heap is the list you want to use as a heap (heap stays as heap bc the list was defined as heap = [])
