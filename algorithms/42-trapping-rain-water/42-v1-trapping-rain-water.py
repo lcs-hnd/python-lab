a973330f1f67de5cc8a20de1924f33e3ce698c20
Lucas Honda <103671864+lcs-hnd@users.noreply.github.com>
2025-08-04 00:18:25 -0400

    docs: rewrite trw

--- algorithms/42-trapping-rain-water/42-v1-trapping-rain-water.py
+++ algorithms/42-trapping-rain-water/42-v1-trapping-rain-water.py
@@ -1,18 +1,24 @@
-class Solution:
-    def trap(self, height: List[int]) -> int:
-        left = 0
-        lm = 0
-        right = len(height) - 1
-        rm = 0
-        bucket = 0
-        while left < right:
-            lm = max(lm,height[left])
-            rm = max(rm,height[right])
-            mbar = min(rm,lm)
-            if height[left] <= height[right]:
-                bucket += max(0,mbar-height[left])
-                left +=1
-            elif height[left] > height[right]:
-                bucket += max(0,mbar-height[right])
-                right -=1
-        return bucket
+# 42 v1 - trapping rain water
+
+height=[4,2,3]
+
+def water(height):
+  left = 0
+  lm = 0
+  right = len(height) - 1
+  rm = 0
+  bucket = 0
+
+  while left < right:
+    lm = max(lm,height[left])
+    rm = max(rm,height[right])
+    mbar = min(lm,rm)
+    if height[left] <= height[right]:
+      bucket += max(0,mbar-height[left])
+      left +=1
+    elif height[left] > height[right]:
+      bucket += max(0,mbar-height[right])
+      right -=1 
+  return bucket
+
+print(water(height))
